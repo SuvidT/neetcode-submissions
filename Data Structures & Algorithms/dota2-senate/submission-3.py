b2d0c3a8e289bfbc9e14d183858a5e95732aaf10
SuvidT <suvid2004@gmail.com>
2026-07-21 15:02:15 -0400

    Add: dota2-senate - submission-3

---
+++ Data Structures & Algorithms/dota2-senate/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        sen = list(senate)
+
+        r = 0
+        d = 0
+
+        i = -1
+        while True:
+            i += 1
+            i %= len(sen)
+
+            if sen[i] == 'R':
+                if d > 0:
+                    sen[i] = '_'
+                    d -= 1
+                else:
+                    r += 1
+            
+            if sen[i] == 'D':
+                if r > 0:
+                    sen[i] = '_'
+                    r -= 1
+                else:
+                    d += 1
+            
+            if sen.count('R') == 0:
+                return 'Dire'
+            if sen.count('D') == 0:
+                return 'Radiant'
