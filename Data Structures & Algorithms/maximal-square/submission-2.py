316d58695d7c197887964ca3efdbeef8b1819ce8
SuvidT <suvid2004@gmail.com>
2026-07-22 10:49:00 -0400

    Add: maximal-square - submission-2

---
+++ Data Structures & Algorithms/maximal-square/submission-2.py
@@ -0,0 +1,42 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        from pprint import pprint
+
+        ROWS = len(matrix)
+        COLS = len(matrix[0])
+
+        best = 0
+        memory = {}
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                val = matrix[i][j]
+                if val == "0":
+                    continue
+
+                sq = 1
+
+                up = 0
+                left = 0
+                up_left = 0
+
+                if (i-1, j) in memory:
+                    up = memory[(i-1, j)]
+
+                if (i, j-1) in memory:
+                    left = memory[(i, j-1)]
+
+                if (i-1, j-1) in memory:
+                    up_left = memory[(i-1, j-1)]
+
+                if up == up_left and up_left == left:
+                    sq += up_left
+                else:
+                    sq += min(up, left, up_left)
+
+                memory[(i, j)] = sq
+                if best < (sq * sq):
+                    best = sq * sq
+
+
+        return best
