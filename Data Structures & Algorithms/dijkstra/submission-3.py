f8bfb8903f3cda9ebdd16b4e667be813aaee110c
SuvidT <suvid2004@gmail.com>
2026-08-20 12:50:18 -0400

    Add: dijkstra - submission-3

---
+++ Data Structures & Algorithms/dijkstra/submission-3.py
@@ -0,0 +1,31 @@
+class Solution:
+    def shortestPath(self, n: int, edges: List[List[int]], src: int) -> Dict[int, int]:
+        import heapq
+        from collections import defaultdict
+    
+        adj = defaultdict(list)
+        for s, e, d in edges:
+            adj[s].append((e, d))
+    
+        dists = {src: 0}
+        queue = [(0, src)]
+    
+        while queue:
+            dist, node = heapq.heappop(queue)
+    
+            if dist > dists[node]:
+                continue
+    
+            for neighbor, weight in adj[node]:
+                new_dist = dist + weight
+    
+                if neighbor not in dists or new_dist < dists[neighbor]:
+                    dists[neighbor] = new_dist
+                    heapq.heappush(queue, (new_dist, neighbor))
+    
+        for s, e, d in edges:
+            if s not in dists:
+                dists[s] = -1
+                
+        return dists
+    
